c19b37417a2ce0d0d02d26e7c21c7854d24fea9f
minhbear <minhbeardev@gmail.com>
2025-04-25 01:44:42 +0700

    chore: update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,15 +20,15 @@ resource "aws_s3_bucket_ownership_controls" "vertex_landing_page_bucket_ownershi
 resource "aws_s3_bucket_public_access_block" "vertex_landing_page_bucket_public_access_block" {
   bucket = aws_s3_bucket.vertex_landing_page_bucket.id
 
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = true
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = true
   restrict_public_buckets = false
 }
 
 # Bucket ACL
 resource "aws_s3_bucket_acl" "vertex_landing_page_bucket_acl" {
-  
+
   depends_on = [
     # full reference of ownership resource ------
     aws_s3_bucket_ownership_controls.vertex_landing_page_bucket_ownership_controls,
@@ -36,7 +36,7 @@ resource "aws_s3_bucket_acl" "vertex_landing_page_bucket_acl" {
   ]
 
   bucket = aws_s3_bucket.vertex_landing_page_bucket.id
-  acl = "public-read"
+  acl    = "public-read"
 }
 
 # Bucket policy
@@ -46,13 +46,13 @@ resource "aws_s3_bucket_policy" "vertex_landing_page_bucket_policy" {
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
-        {
-          Sid = "PublicReadGetObject"
-          Effect = "Allow"
-          Principal = "*"
-          Action = "s3:GetObject"
-          Resource = "${aws_s3_bucket.vertex_landing_page_bucket.arn}/*"
-        }
+      {
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.vertex_landing_page_bucket.arn}/*"
+      }
     ]
   })
 }
@@ -63,24 +63,24 @@ resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
 }
 
 # Cloudfront distribution
-resource "aws_cloudfront_distribution" "vertex_landing_page_distribution" { 
+resource "aws_cloudfront_distribution" "vertex_landing_page_distribution" {
   origin {
     domain_name = aws_s3_bucket.vertex_landing_page_bucket.bucket_regional_domain_name
-    origin_id = "S3-vertex-landing-page-bucket"
-    
+    origin_id   = "S3-vertex-landing-page-bucket"
+
     s3_origin_config {
       origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
     }
   }
 
-  enabled = true
-  is_ipv6_enabled = true
-  comment = "Vertex Landing Page"
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Vertex Landing Page"
   default_root_object = "index.html"
 
   default_cache_behavior {
-    allowed_methods = ["GET", "HEAD", "OPTIONS"]
-    cached_methods = ["GET", "HEAD"]
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
     target_origin_id = "S3-vertex-landing-page-bucket"
 
     forwarded_values {
@@ -91,9 +91,9 @@ resource "aws_cloudfront_distribution" "vertex_landing_page_distribution" {
     }
 
     viewer_protocol_policy = "redirect-to-https"
-    min_ttl = 0
-    default_ttl = 3600
-    max_ttl = 86400
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
   restrictions {
